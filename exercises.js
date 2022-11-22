e162a61630fdb5ef30450cee517a51f8a3b6ffc2
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 15:23:47 -0600

    FizzBuzz exercise.

--- exercises.js
+++ exercises.js
@@ -14,3 +14,19 @@ console.log(isLandscape(1,2))
 function isLandscape(width, height) {
     return (width > height);
 }
+
+// #3 FizzBuzz
+
+console.log(fizzBuzz('p'));
+
+function fizzBuzz(input) {
+    if (!Number(input)) return 'Not a number';
+
+    if (input % 15 === 0) return 'FizzBuzz';
+
+    if (input % 3 === 0) return 'Fizz';
+
+    if (input % 5 === 0) return 'Buzz';
+
+    return input;
+}
