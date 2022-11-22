c82f99391afe0e0d5bc586862a776b94f1ae241d
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 13:25:03 -0600

    Learning if-else.

---
+++ flow.js
@@ -0,0 +1,14 @@
+
+// If hour is between 6am and 12pm: Good morning!
+// If hour is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evening!
+
+let hour = 13;
+
+if (hour >= 6 && hour < 12) 
+    console.log('Good morning!')
+else if (hour >= 12 && hour < 18) 
+    console.log('Good afternoon!')
+else 
+    console.log('Good evening!')
+
