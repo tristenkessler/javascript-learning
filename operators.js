0ca26759967ea5848c50f5e103539d1e17e83aa6
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 12:38:24 -0600

    Bitwise operators.

--- operators.js
+++ operators.js
@@ -75,3 +75,20 @@ let defaultColor = 'blue';
 let currentColor = userColor || defaultColor;
 
 console.log(currentColor);
+
+
+console.log(1 | 2);
+console.log(1 & 2);
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+console.log(myPermission);
+
+let message = (myPermission & readPermission) ? 'yes' : 'no';
+
+console.log(message);
+
