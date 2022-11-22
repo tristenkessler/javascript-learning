8196bbfd8e60067208b0117173aa60b0fa82d028
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 11:01:31 -0600

    Learn about primitives.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log('Hello World');
 
 let name = 'Tristen';
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
 console.log(name);
 
 const interestRate = 0.3;
