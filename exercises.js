d6d47f6ab5920d88e529a6a3ccc7306976e38e9c
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 17:02:19 -0600

    Sum of multiples of 3 and 5 exercise.

--- exercises.js
+++ exercises.js
@@ -90,3 +90,19 @@ function showProperties(obj) {
         if (typeof movie[property] === 'string') console.log(property, movie[property]);
     }
 }
+
+// Sum of multiples of 3 and 5
+
+console.log(sum(33));
+
+function sum(limit) {
+    
+    let answer = 0;
+
+    for(i = 0; i <= limit; i++){
+        if (i % 3 === 0 || i % 5 === 0) 
+            answer += i;
+    }
+    return answer;
+}
+
