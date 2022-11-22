12437ee43eca50a9d7e8bd5cfc1317c4f30de31a
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 11:52:43 -0600

    Learn arithmetic operators.

---
+++ operators.js
@@ -0,0 +1,20 @@
+
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Increment (++)
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+// Decrement (--)
+console.log(--x);
+console.log(x--);
+console.log(x);
