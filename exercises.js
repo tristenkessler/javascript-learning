6620619b964b74836de16525a36c06ea6599b742
Tristen Kessler <t3kessler@gmail.com>
2022-11-22 14:58:00 -0600

    Return if picture is landscape.

--- exercises.js
+++ exercises.js
@@ -6,3 +6,11 @@ console.log(number);
 function max(numOne, numTwo) {
     return (numOne > numTwo) ? numOne: numTwo;
 }
+
+// #2 is landscape or portrait
+
+console.log(isLandscape(1,2))
+
+function isLandscape(width, height) {
+    return (width > height);
+}
