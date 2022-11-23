fdee63b12c20a5ade9830e00a9f430b1281d5468
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 10:12:10 -0600

    Learning basics of objects.

---
+++ objects.js
@@ -0,0 +1,15 @@
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVariable: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
+
