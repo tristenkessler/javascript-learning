89ec87a48120957dd27ffa15fff69b7a3b71462c
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 10:22:42 -0600

    Learn factory functions.

--- objects.js
+++ objects.js
@@ -1,15 +1,16 @@
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVariable: true,
-    draw: function() {
-        console.log('draw');
-    }
-};
+function createCircle(radius) {
+    return {
+        radius, // = radius: radius,
+        draw() { // = draw: function() {}
+            console.log('draw');
+        }
+    };
+}
 
-circle.draw();
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
 
