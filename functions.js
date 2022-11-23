e787aa353b8912874e138ef86b00458133bc7227
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 16:30:44 -0600

    Try and catch.

--- functions.js
+++ functions.js
@@ -38,12 +38,23 @@ const person1 = {
         return `${person1.firstName} ${person1.lastName}`
     },
     set fullName(value) {
+        if (typeof value !== 'string') 
+            throw new Error('Value is not a string.');
         const parts = value.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name')
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 };
 
-person1.fullName = 'John Smith';
+try {
+    person1.fullName = '';
+}
+catch (e) {
+    alert(e);
+}
 
 console.log(person1);
+
+
