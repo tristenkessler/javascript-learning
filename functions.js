2e4f2306f8b89f114f25a39e194d8c70f2ad5372
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 15:59:39 -0600

    Explore arguments.

--- functions.js
+++ functions.js
@@ -9,3 +9,12 @@ let run = function() {
 move = run;
 run();
 move();
+
+function sum() {
+    let total = 0;
+    for (let value of arguments)
+        total += value
+    return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6))
