5c7b68ae923e9d3886bd853d10d995384ae9b6a9
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 13:20:25 -0600

    Find elements in array.

--- arrays.js
+++ arrays.js
@@ -11,3 +11,23 @@ numbers.unshift(1, 2);
 numbers.splice(2, 0, 'a', 'b')
 
 console.log(numbers)
+
+// Finding Elements
+
+const numbers1 = [1, 2, 3, 4];
+console.log(numbers1.indexOf('1'));
+console.log(numbers1.indexOf(1, 2));
+
+console.log(numbers1.includes(1));
+
+const courses = [
+    {id: 1, name: 'a'},
+    {id: 2, name: 'b'},
+];
+
+const course = courses.findIndex(function(course) {
+    return course.name === 'a';
+});
+
+console.log(course)
+
