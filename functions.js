96c4775897e9ad0b2447b05c0b1e8a242038806d
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 16:10:01 -0600

    Explore default values.

--- functions.js
+++ functions.js
@@ -24,3 +24,8 @@ function sum(discount, ...prices) {
 console.log(sum(0.1, 20, 30))
 
 
+function interest(principal, rate = 3.5, years = 5) {
+    return principal * rate / 100 * years;
+}
+
+console.log(interest(10000))
