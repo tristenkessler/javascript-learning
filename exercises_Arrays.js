e2bad221cc6e66c3e58791e9774e1958a38619da
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 15:42:49 -0600

    Move an element exercise.

--- exercises_Arrays.js
+++ exercises_Arrays.js
@@ -38,3 +38,22 @@ function except(array, excludes) {
             output.push(element);
     return output;
 }
+
+// #4 Moving an Element
+
+const output1 = move(numbers3, 0, 7)
+
+console.log(output1);
+
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0) {
+        console.error('Invalid offset.');
+        return;
+    }
+
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(index + offset, 0, element);
+    return output;
+}
