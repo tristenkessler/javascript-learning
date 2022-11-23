e27857cbfe22acb277b7a4a642113dacd9add2c9
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 10:28:23 -0600

    Constructor Function.

--- objects.js
+++ objects.js
@@ -14,3 +14,14 @@ console.log(circle1);
 const circle2 = createCircle(2);
 console.log(circle2);
 
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
+const x = {};
