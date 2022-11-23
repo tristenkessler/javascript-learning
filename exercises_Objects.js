6dc38aa415ac0bff71d433df4aa725981037102c
Tristen Kessler <t3kessler@gmail.com>
2022-11-23 12:21:44 -0600

    Built a constructor function.

--- exercises_Objects.js
+++ exercises_Objects.js
@@ -19,3 +19,15 @@ let myAddress = createAddress('My Street', 'Des Moines', 50316);
 
 showAddress(myAddress);
 
+// #2 Factory and Constructor Functions
+// Factory ^^^
+
+// Constructor Function
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+const myNewAddress = new Address('My Street', 'Des Moines', 50316);
+
+console.log(myNewAddress);
