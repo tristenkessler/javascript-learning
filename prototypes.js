2c7ecb231dd51a1a63f48ab245d756fde7a8dd54
Tristen Kessler <t3kessler@gmail.com>
2022-11-26 13:43:55 -0600

    Prototypical inheritance.

--- prototypes.js
+++ prototypes.js
@@ -27,3 +27,38 @@ const array = [];
 array.shuffle();
 // Don't modify objects you don't own!!!
 
+// *** PROTOTYPICAL INHERITANCE ***
+
+function Shape(color) {
+    this.color = color;
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
+function Circle(radius, color) {
+    Shape.call(this,color);
+
+    this.radius = radius;
+}
+
+extent(Circle, Shape);
+
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+function Square(size) {
+    this.size = size;
+}
+
+extent(Square, Shape);
+
+const s = new Shape;
+const c = new Circle(1, 'red');
